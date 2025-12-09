993a5a5abc1edc2dad1715acdfae1761e879a854
flybol <lovelookyou@126.com>
2025-12-09 22:30:41 +0800

    给删除接口添加注释

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,25 +1,50 @@
+from pydantic import BaseModel
+from fastapi import FastAPI
 
-from  fastapi import FastAPI
+
 def create_app() -> FastAPI:
     app = FastAPI()
     return app
 
+
 app = create_app()
+
+
 @app.get("/ping")
 def ping():
     return {"status": "ok"}
 
 
 mock_db = [
-        {"id": 1, "name": "John"},
-        {"id": 2, "name": "Jane"},
-        {"id": 3, "name": "Jim"},
-    ]
+    {"id": 1, "name": "John"},
+    {"id": 2, "name": "Jane"},
+    {"id": 3, "name": "Jim"},
+]
+
+
 @app.get("/users")
 def get_users():
     return mock_db
 
+
 @app.delete("/users/{user_id}")
 def remove_user(user_id: int):
+    """Delete a user"""
     global mock_db
     return [user for user in mock_db if user["id"] != user_id]
+
+
+class UpdateUser(BaseModel):
+    name: str | None = None
+
+
+@app.patch("/users/{user_id}")
+def update_user(user_id: int, user: UpdateUser):
+    global mock_db
+    user_data = user.model_dump(exclude_unset=True)
+
+    for item in mock_db:
+        if item["id"] == user_id:
+            item.update(user_data)
+            return item
+    return {"message": "User not found"}
